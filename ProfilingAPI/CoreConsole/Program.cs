0412a6be18a460f7bbb69d996fb407d1609e377f
Mark Fawaz <markfawa@amazon.com>
2021-09-22 11:15:51 -0700

    Add sample application and batch file to run

---
+++ ProfilingAPI/CoreConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CoreConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Read();
+        }
+    }
+}
---
+++ ProfilingAPI/StartCore.bat
@@ -0,0 +1,5 @@
+SET CORECLR_PROFILER={cf0d821e-299b-5307-a3d8-b283c03916dd}
+SET CORECLR_ENABLE_PROFILING=1
+SET CORECLR_PROFILER_PATH=ELTProfiler\x64\Debug\ClrProfiler.dll
+
+"CoreConsole\bin\Debug\netcoreapp3.1\CoreConsole.exe"
